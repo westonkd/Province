32cab029d51d2adb2f9f4874cd27ea5430074992
cameronthomas <cameronthomas@10.37.155.74>
2014-06-20 12:06:57 -0600

    Test

--- ProvincialMiner/src/provincialminer/ProvincialMiner.java
+++ ProvincialMiner/src/provincialminer/ProvincialMiner.java
@@ -28,7 +28,7 @@ public class ProvincialMiner extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello Weston!");
             }
         });
         
